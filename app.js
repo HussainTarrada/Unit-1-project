45f1cb759fef60dcae227b620c5f1d218cf10179
Hussain Tarrada <hussaintarrada@Hussains-MacBook-Air.local>
2024-12-08 22:20:48 +0300

    Starting with Javascript

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-let fruits = ["Banana", "Apple", "Orange", "Blueberry", "Avocado", "Grapes", "Lemon", "Tomato", "Potato", "Dates", "Watermelon", "Coconut", "Mushrooms", "Strawberry", "Mango", ""]
+let fruits = ["Banana", "Apple", "Orange", "Blueberry", "Avocado", "Grapes", "Lemon", "Tomato", "Potato", "Dates", "Watermelon", "Coconut", "Mushrooms", "Strawberry", "Mango", "Cherry", "Pineapple"]
+
+const randomElement = fruits[Math.floor(Math.random() * fruits.length)]
+
+console.log(randomElement)
 
 let mode = ""
-
-function 
-
-
-
 
 
