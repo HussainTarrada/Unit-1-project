43cade6d2250475d7d47994b6f7d7d290c159b65
Hussain Tarrada <hussaintarrada@Hussains-MacBook-Air.local>
2024-12-08 22:40:22 +0300

    Create a function to make spaces of the word

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 let fruits = ["Banana", "Apple", "Orange", "Blueberry", "Avocado", "Grapes", "Lemon", "Tomato", "Potato", "Dates", "Watermelon", "Coconut", "Mushrooms", "Strawberry", "Mango", "Cherry", "Pineapple"]
 
-const randomElement = fruits[Math.floor(Math.random() * fruits.length)]
+const myFruit = fruits[Math.floor(Math.random() * fruits.length)]
 
-console.log(randomElement)
+console.log(myFruit)
+
+console.log(myFruit.length)
 
 let mode = ""
 
+const letterElement = document.querySelector("#Letters")
 
+function myWord(){
+    for(let i=0; i<myFruit.length; i++){
+        const word = document.createElement("p");
+        const space = document.createTextNode("_")
+        word.appendChild(space)
+    }
+}
+
+myWord()
