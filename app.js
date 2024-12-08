9a05d50137873790e34c2cf5dfcf5b35669dfca4
Hussain Tarrada <hussaintarrada@Hussains-MacBook-Air.local>
2024-12-08 22:10:33 +0300

    continue with HTML and CSS

---
+++ app.js
@@ -0,0 +1,10 @@
+let fruits = ["Banana", "Apple", "Orange", "Blueberry", "Avocado", "Grapes", "Lemon", "Tomato", "Potato", "Dates", "Watermelon", "Coconut", "Mushrooms", "Strawberry", "Mango", ""]
+
+let mode = ""
+
+function 
+
+
+
+
+
