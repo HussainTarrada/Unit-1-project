d380dffb4618d56f1088e084feff967816f53db4
Hussain Tarrada <hussaintarrada@Hussains-MacBook-Air.local>
2024-12-10 18:57:22 +0300

    Modify more in HTML & CSS, splitting the word into seperate letters

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let fruits = ["Banana", "Apple", "Orange", "Blueberry", "Avocado", "Grapes", "Lemon", "Tomato", "Potato", "Dates", "Watermelon", "Coconut", "Mushrooms", "Strawberry", "Mango", "Cherry", "Pineapple"]
 
-
+let myFruit = ""
 
 let mode = ""
 
@@ -8,16 +8,37 @@ const letterElement = document.querySelector("#Letters")
 
 const myStartButton = document.querySelector("#startButton")
 
+const myAlphabets = document.querySelectorAll(".alphabet")
+
 
 function myWord(){
-    const myFruit = fruits[Math.floor(Math.random() * fruits.length)]
+    if(myFruit !== ""){
+        return
+    }
+    myFruit = fruits[Math.floor(Math.random() * fruits.length)]
+
     console.log(myFruit)
     console.log(myFruit.length)
     for(let i=0; i<myFruit.length; i++){
-        var div = document.createElement("div")
-        div.setAttribute('class', 'spaces')
+        let div = document.createElement("div")
+        div.innerText = " ___ "
+        letterElement.appendChild(div)
     }
+   wordSplitter()
+}
+
+function wordSplitter(){
+    const myFruitArray = myFruit.split( "" )
+    console.log(myFruitArray)
+    
+
 }
 
 
 myStartButton.addEventListener("click", myWord)
+
+myAlphabets.forEach((letter)=>{
+    letter.addEventListener("click", 
+        console.log(letter)
+    )
+})
